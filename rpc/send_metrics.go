eabf786d0030fee8a96b24256fd0a009851bc370
Betim <gvampre1991@gmail.com>
2022-04-10 15:44:19 -0300

    Update rpc interface

--- rpc/send_metrics.go
+++ rpc/send_metrics.go
@@ -7,25 +7,40 @@ import (
 
 type Rpc struct{}
 
-type Args struct {
-	Message string
-	Context string
+type Event struct {
+	Data interface{}
+	Name string
 }
 
 type Reply struct {
 	FinalMessage string
 }
 
-func (r *Rpc) SendMetric(message string) {
-	client, err := rpc.DialHTTPPath("tcp", "127.0.0.1:8080", "/rpc")
-	if err != nil {
-		log.Fatal("dialing:", err)
+var client *rpc.Client
+
+func getClient() (*rpc.Client, error) {
+	if client == nil {
+		c, err := rpc.DialHTTPPath("tcp", "127.0.0.1:8080", "/rpc")
+		if err != nil {
+			log.Println("error dialing:", err)
+			return c, err
+		}
+		client = c
 	}
 
-	a := Args{Message: message, Context: "sysperf"}
-	var repl Reply
+	return client, nil
+}
 
-	replyCall := client.Call("Rpc.SendMetric", a, &repl)
+func (r *Rpc) SendMetric(payload interface{}, metricName string) error {
+	c, err := getClient()
+	if err != nil {
+		return err
+	}
+	e := Event{Data: payload, Name: metricName}
+	repl := new(Reply)
+
+	replyCall := c.Call("Rpc.SendMetric", e, repl)
 	log.Printf("%+v", replyCall)
 	log.Printf("%+v", repl)
+	return nil
 }
