592ae5f9f6edfb1033d937582425710a1a8c0a04
Nathan S. Martins <nathan.eua@gmail.com>
2022-05-19 08:20:21 -0300

    cleanup

--- perf.go
+++ perf.go
@@ -1,13 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-	"net/http"
 	"os"
 	"path/filepath"
 	"time"
@@ -42,49 +35,6 @@ type PerfFile struct {
 			Dso    string `json:"dso,omitempty"`
 		} `json:"callchain"`
 	} `json:"samples"`
-}
-
-func SendMetric(event interface{}, eventName string) error {
-	body, err := json.Marshal(event)
-	if err != nil {
-		log.Println("error marshaling event:", err)
-		return err
-	}
-
-	sysPerfUrl, found := os.LookupEnv("SERVER_URL")
-
-	if !found {
-		log.Println("WARNING: SERVER_URL env var not found, using default: localhost:8080")
-		sysPerfUrl = "http://localhost:8080/metric"
-	}
-
-	payload := bytes.NewBuffer(body)
-	req, err := http.NewRequest(http.MethodPost, sysPerfUrl, payload)
-	if err != nil {
-		log.Println("new request error:", err)
-		return err
-	}
-
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("X-Event-Name", eventName)
-
-	c := &http.Client{}
-	res, err := c.Do(req)
-	if err != nil {
-		return fmt.Errorf("sysperf api error: %s", err)
-	}
-	defer func(Body io.ReadCloser) {
-		_ = Body.Close()
-	}(res.Body)
-
-	response, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return fmt.Errorf("sysperf api error: %s", err)
-	}
-
-	log.Printf("API response: %s\n", response)
-
-	return nil
 }
 
 func FileCleanUp(pattern string) error {
