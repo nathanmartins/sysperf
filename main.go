e0cc423007c91b4ae804cb0f8cede942ed146cea
Nathan S. Martins <nathan.eua@gmail.com>
2022-06-26 19:19:02 -0300

    using structered logging

--- main.go
+++ main.go
@@ -1,29 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"github.com/nathanmartins/sysperf/collectors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"log"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"net/http"
 )
 
 func init() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	var memCollector collectors.MeminfoCollector
-
-	// This will register fine.
 	if err := prometheus.Register(&memCollector); err != nil {
-		fmt.Println(err)
+		log.Err(err)
 	} else {
-		fmt.Println("memcollector registered.")
+		log.Info().Msg("memcollector registered.")
 	}
 }
 
 func main() {
-	// The Handler function provides a default handler to expose metrics
-	// via an HTTP server. "/metrics" is the usual endpoint for that.
 	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
+	log.Info().Msg("Beginning to serve on port :8080")
+	log.Err(http.ListenAndServe(":8080", nil))
 }
